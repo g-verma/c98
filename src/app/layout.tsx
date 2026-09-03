2b567dca90c04069cf7e63bff139450723bd98a6
g-verma <129998775+g-verma@users.noreply.github.com>
2026-09-03 12:23:16 +0530

    mobile layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full overflow-hidden antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col overflow-hidden">{children}</body>
     </html>
   );
 }
